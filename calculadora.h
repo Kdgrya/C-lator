3f281aa362fcf479743809c2494ab42f408acbad
кдря <49372253+Kdgrya@users.noreply.github.com>
2026-08-18 18:14:08 -0400

    First Commit

---
+++ calculadora.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+void suma ( n1, n2){
+	printf( n1 + n2);
+	return 0;
+
+void resta( n1, n2){
+	printf( n1 - n1);
+	return 0;
+}
+
+void multiplicacion ( n1, n2){
+	printf( n1 * n2);
+	return 0;
+}
+
+void division ( n1, n2) {
+	printf( n1 / n2);
+	return 0;
+}
